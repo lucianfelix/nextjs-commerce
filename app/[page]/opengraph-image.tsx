7c04f48962f96ea180e6b0b3a3e78c3fbb3dbf79
Ghita Lucian <ghita@adobe.com>
2023-08-17 18:40:42 +0200

    simpler

--- app/[page]/opengraph-image.tsx
+++ app/[page]/opengraph-image.tsx
@@ -1,10 +1,11 @@
 import OpengraphImage from 'components/opengraph-image';
 import { getPage } from 'lib/shopify';
+import { Page } from '../../lib/shopify/types';
 
 export const runtime = 'edge';
 
 export default async function Image({ params }: { params: { page: string } }) {
-  const page = await getPage(params.page);
+  const page = (await getPage(params.page)) as Page;
   const title = page.seo?.title || page.title;
 
   return await OpengraphImage({ title });
