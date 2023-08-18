4192d711eda71d6eefd658860ba72b848f825ae0
Ghita Lucian <ghita@adobe.com>
2023-08-18 14:04:20 +0200

    bump

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
   description: 'Search for products in the store.'
 };
 
-export const revalidate = 43200;
+export const revalidate = 33200;
 
 export default async function SearchPage({
   searchParams
